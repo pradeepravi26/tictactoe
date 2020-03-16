2746e064e431bd595980b7901d1d781c5035f69f
Pradeep Ravi <pradeepravi2006@gmail.com>
2020-03-16 09:40:31 -0400

    added logic to identify which quadrant was clicked

--- tictactoe.py
+++ tictactoe.py
@@ -5,6 +5,7 @@
 import pygame
 import sys
 import random
+import math
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
@@ -18,6 +19,19 @@ white = (255,255,255)
 crashed = False
 clock = pygame.time.Clock()
 game_board = pygame.image.load('tic-tac-toe-board-png-2.png')
+x_image = pygame.image.load('tictactoe_x.png')
+o_image = pygame.image.load('tictactoe_o.png')
+p1 = True
+p2 = True
+
+def findQuadrant(xy):
+    x = xy[0]
+    y = xy[1]
+    column = (x - 109)/187
+    row = (y - 42)/165
+    quadrant = (row - 1) * 3 + column
+    return quadrant
+
 
 # One iter of this while loop = basically 1 frame
 while not crashed:
@@ -28,12 +42,13 @@ while not crashed:
             crashed = True
 
     # If it should run every frame it should be under this
-    
+        if pygame.mouse.get_pressed()[0] == 1:
+            print(pygame.mouse.get_pos())
+            print(math.ceil(findQuadrant(pygame.mouse.get_pos())))
 
     # All drawing should be under this
     gameDisplay.fill(blue)
     gameDisplay.blit(game_board, (0, 0)) 
-
 
 
     pygame.display.update()
